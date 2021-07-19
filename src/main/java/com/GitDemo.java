cabfbcd7b535d3c33e9c5d7ec5d4ba3c78b5240f
lee <lee@qq.com>
2021-07-19 14:52:04 +0800

    second commit

--- src/main/java/com/GitDemo.java
+++ src/main/java/com/GitDemo.java
@@ -6,5 +6,7 @@ public class GitDemo {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
+        System.out.println("sorry git");
+
     }
 }
